e9435c48e4ab66c27cd6f0ce5bf5209165cd71be
Raphael Mansueto <raphaelmanswork@gmail.com>
2022-02-18 14:23:51 +0800

    Initial implementation for tip by reply feature

--- callback_handlers/confirm-transaction.ts
+++ callback_handlers/confirm-transaction.ts
@@ -3,14 +3,16 @@ import { CallbackData } from "./enums";
 import { CallbackHandler } from "./types";
 import web3 from "services/web3";
 import { callbackUtils } from "callback_handlers";
+import { WalletService } from "services/wallet-service";
 
 export class ConfirmTransactionCallbackHandler implements CallbackHandler {
+  constructor(private walletService: WalletService) {}
   callbackData = CallbackData.ConfirmTransaction;
   explorerLink = process.env.EXPLORER_LINK ?? "https://explorer.syscoin.org";
 
   async handleCallback(bot: TelegramBot, update: Update): Promise<void> {
     callbackUtils.removeInlineKeyboardOptions(bot, update);
-    const { message, id } = update.callback_query!;
+    const { message, id, from } = update.callback_query!;
     const sendMessageConfig: SendMessageOptions = {
       parse_mode: "Markdown",
     };
@@ -27,10 +29,19 @@ export class ConfirmTransactionCallbackHandler implements CallbackHandler {
 
     const sendEvent = web3.eth.sendSignedTransaction(rawTransaction);
 
-    sendEvent.on("sent", (payload) => {
-      bot.answerCallbackQuery(id, {
-        text: `The transaction has been sent to the network`,
-      });
+    const addressLink = `${this.explorerLink}/address`;
+    sendEvent.on("sent", async (_) => {
+      const username = from.username!;
+      const walletAddress = (await this.walletService.getWallet(username))
+        ?.address;
+      await bot.sendMessage(
+        chatId,
+        `The transaction has been sent to the network.\n[Check pending transactions](${addressLink}/${walletAddress})`,
+        {
+          parse_mode: "Markdown",
+          disable_web_page_preview: true,
+        }
+      );
     });
 
     sendEvent.on("error", (err) => {
@@ -43,9 +54,10 @@ export class ConfirmTransactionCallbackHandler implements CallbackHandler {
       const txHash = receipt.transactionHash;
       bot.sendMessage(
         chatId,
-        `Transaction was successful\n\nTxHash: ${txHash}\n\n Open in [explorer](${txLink}/${txHash})`,
+        `Transaction was successful\n\nTxHash: ${txHash}\n\nOpen in [explorer](${txLink}/${txHash})`,
         {
           parse_mode: "Markdown",
+          disable_web_page_preview: true,
         }
       );
     });
--- message_handlers/group/index.ts
+++ message_handlers/group/index.ts
@@ -6,14 +6,29 @@ import { tip } from "./tip";
 export const handleGroupMessage = async (bot: TelegramBot, update: Update) => {
   const { text, group_chat_created } = update.message!;
   console.log("Group Message Text:", text);
-  if (group_chat_created) {
-    return invitedToGroup(bot, update);
+
+  const isBotInvited = await isBotNewlyInvited(bot, update);
+  if (group_chat_created || isBotInvited) {
+    return await invitedToGroup(bot, update);
   }
   const command = text ?? "";
   if (/\/help/g.test(command)) {
     await help(bot, update);
-  } else if (/\/tip/g) {
+  } else if (/\/tip/g.test(command)) {
     await tip(bot, update);
   }
   return;
 };
+
+async function isBotNewlyInvited(bot: TelegramBot, update: Update) {
+  const { new_chat_members } = update.message!;
+  if(!new_chat_members){
+    return false;
+  }
+  const botUsername = (await bot.getMe()).username!;
+  const isBotInvited = new_chat_members.find(
+    (member) => member.username === botUsername
+  );
+
+  return isBotInvited;
+}
